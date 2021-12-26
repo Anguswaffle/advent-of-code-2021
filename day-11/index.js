3cd3506299b5af1264f24c17f9ea94fe8fb24759
Anguswaffle <90980870+Anguswaffle@users.noreply.github.com>
2021-12-25 21:37:36 -0600

    Day 11

--- day-11/index.js
+++ day-11/index.js
@@ -1,4 +1,15 @@
-const testInput = `7222221271
+const testInput = `5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526`.split('\n').map(line => line.split('').map(num => parseInt(num, 10)));
+
+const input = `7222221271
 6463754232
 3373484684
 4674461265
@@ -9,3 +20,74 @@ const testInput = `7222221271
 5286325337
 5771324832`.split('\n').map(line => line.split('').map(num => parseInt(num, 10)));
 
+
+const isFlashed = (currentState) => currentState === 0
+
+const findAdjacent = (map, x, y) => {
+  return [
+    [x, y - 1],
+    [x + 1, y - 1],
+    [x + 1, y],
+    [x + 1, y + 1],
+    [x, y + 1],
+    [x - 1, y + 1],
+    [x - 1, y],
+    [x - 1, y - 1]
+  ].filter(([x, y]) => y in map && x in map[y]);
+}
+
+const flashOctos = octopusArr => {
+  let flashCount = 0;
+  let cycles = 0;
+  while (true) {
+
+    for (let x = 0; x < 10; x++) {
+      for (let y = 0; y < 10; y++) {
+        octopusArr[y][x]++;
+      }
+    }
+
+    while (octopusArr.flat(2).find(element => element > 9)) {
+      for (let x = 0; x < 10; x++) {
+        for (let y = 0; y < 10; y++) {
+          if (octopusArr[y][x] > 9) {
+            octopusArr = flashSurround(octopusArr, x, y)
+            octopusArr[y][x] = 'flash';
+          }
+        }
+      }
+    }
+
+    for (let x = 0; x < 10; x++) {
+      for (let y = 0; y < 10; y++) {
+        if (octopusArr[y][x] === 'flash') {
+          octopusArr[y][x] = 0;
+          flashCount++
+        }
+      }
+    }
+
+    // For part 2 only. Comment this line out to get part 1 solution
+    if(octopusArr.flat(2).every(isFlashed)) return ++cycles;
+
+
+    
+    cycles++;
+  }
+  return flashCount
+}
+
+const flashSurround = (octopusArr, x, y) => {
+  const surroundingOcto = findAdjacent(octopusArr, x, y);
+  for (const [x, y] of surroundingOcto) {
+    if (octopusArr[y][x] !== 'flash') octopusArr[y][x]++;
+  }
+  return octopusArr;
+}
+
+const init = octopusArr => {
+  const count = flashOctos(octopusArr);
+  console.log(`The answer to part one is ${count}.`)
+}
+
+init(input)
